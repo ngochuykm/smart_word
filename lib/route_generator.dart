e8d8744f441aac7988f635b14c566e722de9b462
bangphan <bang.phan.596@gmail.com>
2021-01-25 17:39:57 +0700

    up code

--- lib/route_generator.dart
+++ lib/route_generator.dart
@@ -1,13 +1,14 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+// import 'src/models/route_argument.dart';
 
-class RouteGenerator {
-  static Route<dynamic> generateRoute(RouteSettings settings) {}
-  final args = settings.arguments;
-  // switch (settings.name) {
-  //   case :
-      
-  //     break;
-  //   default:
-  // }
-
-}
+// class RouteGenerator {
+//   static Route<dynamic> generateRoute(RouteSettings settings) {
+//     final args = settings.arguments;
+//     switch (settings.name) {
+//       case '/Splash':
+//         return MaterialPageRoute(builder: (_) => SplashScreen());
+//       default:
+//         return MaterialPageRoute(builder: (_) => Scaffold(body: SafeArea(child: Text('Route Error'))));
+//     }
+//   }
+// }
---
+++ lib/src/pages/splash_screen.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+// import 'package:mvc_pattern/mvc_pattern.dart';
+
+import '../controllers/splash_screen_controller.dart';
