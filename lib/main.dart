479b596a98ab82f24efcad3c5373e1b7ad636163
bangphan <bang.phan.596@gmail.com>
2021-01-26 08:44:32 +0700

    up code

--- lib/main.dart
+++ lib/main.dart
@@ -51,23 +51,44 @@ class _SplashScreenState extends State<SplashScreen>{
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 50.0,
-                child: Icon(
-                  Icons.shopping_cart,
-                  color: Colors.greenAccent,
-                  size: 50.0,
-                ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                ),
-                Text(
-                  "Smart Word", style: TextStyle(
-                    color: Colors.white, fontSize: 24.0,
-                    fontWeight: FontWeight.bold
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment:  MainAxisAlignment.center,
+                    children: <Widget>[
+                      CircleAvatar(
+                      backgroundColor: Colors.white,
+                      radius: 50.0,
+                      child: Icon(
+                        Icons.shopping_cart,
+                        color: Colors.greenAccent,
+                        size: 50.0,),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+                      ),
+                      Text(
+                        "Smart Word", style: TextStyle(
+                          color: Colors.white, fontSize: 24.0,
+                          fontWeight: FontWeight.bold
+                        ),
                   )
+                    ],
+                ),
+              ),
+              ),
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircularProgressIndicator(),
+                    Text( "Working Hard"  ,
+                    style:  TextStyle(color: Colors.white ,fontSize: 18.0,fontWeight: FontWeight.bold)
+                    )
+                  ]
+                ),
               ),
             ],
           )
