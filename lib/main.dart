41887e20aec28f4b4ad3dfa3398c300f0ef03582
huy <fromhd.1990@gmail.com>
2021-01-26 08:52:05 +0700

    up code

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,16 @@ import 'package:flutter/rendering.dart';
 import 'package:global_configuration/global_configuration.dart';
 import 'src/helpers/custom_trace.dart';
 import 'route_generator.dart';
+
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  // WidgetsFlutterBinding.ensureInitialized();
   // await GlobalConfiguration().loadFromAsset("configurations");
   // print(CustomTrace(StackTrace.current, message: "base_url: ${GlobalConfiguration().getString('base_url')}"));
   // print(CustomTrace(StackTrace.current, message: "api_base_url: ${GlobalConfiguration().getString('api_base_url')}"));
-  
+
   runApp(MyApp());
 }
+
 class MyApp extends StatefulWidget {
   @override
   _MyAppState createState() => _MyAppState();
@@ -26,53 +28,51 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
+      theme:
+          ThemeData(primaryColor: Colors.red, accentColor: Colors.yellowAccent),
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
-
 }
 
-class SplashScreen extends StatefulWidget{
+class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => new _SplashScreenState();
 }
-class _SplashScreenState extends State<SplashScreen>{
-  @override 
-  Widget build(BuildContext context){
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
+        body: Stack(fit: StackFit.expand, children: <Widget>[
+      Container(
+        decoration: BoxDecoration(color: Colors.redAccent),
+      ),
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(color: Colors.redAccent),
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 50.0,
+            child: Icon(
+              Icons.shopping_cart,
+              color: Colors.greenAccent,
+              size: 50.0,
+            ),
           ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                backgroundColor: Colors.white,
-                radius: 50.0,
-                child: Icon(
-                  Icons.shopping_cart,
-                  color: Colors.greenAccent,
-                  size: 50.0,
-                ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10.0),
-                ),
-                Text(
-                  "Smart Word", style: TextStyle(
-                    color: Colors.white, fontSize: 24.0,
-                    fontWeight: FontWeight.bold
-                  )
-              ),
-            ],
-          )
-        ]
+          Padding(
+            padding: EdgeInsets.only(top: 10.0),
+          ),
+          Text(
+            "Smart Word",
+            style: TextStyle(
+                color: Colors.white,
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold),
+          ),
+        ],
       )
-    );
+    ]));
   }
 }
